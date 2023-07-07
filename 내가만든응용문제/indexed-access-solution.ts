48f61d4f0f2b03caecf2df4bc690ee11820d2fd9
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2023-07-07 15:45:56 +0900

    Create indexed-access-solution.ts

---
+++ 내가만든응용문제/indexed-access-solution.ts
@@ -0,0 +1,25 @@
+// indexed access 응용문제
+
+// 어떤 대학 동아리의 회원 정보를 담은 배열은 아래와 같습니다:
+const Users = [
+  { name: "Alice", age: 22, major: "architecture", college: "engineering" },
+  { name: "Bob", age: 21, major: "nursing", college: "medical" },
+  { name: "Eve", age: 25, major: "computer science", college: "engineering" },
+  { name: "Dean", age: 20, major: "psychology", college: "social sciences" },
+  { name: "Sara", age: 20, major: "veterinary medicine", college: "medical" },
+];
+
+// 이 배열을 활용하여 만든 Student type은 학생 정보 타입입니다. 타입 에러가 발생하지 않도록 적절한 Student 타입을 작성해주세요. 
+const student: Student = {
+    name: "Mason",
+    age: 20,
+    major: "mathematical",
+    college: "natural sciences",
+}
+
+
+// ------ 정답코드 ------
+
+// indexed access는 임의의 타입을 number로 인덱싱해서 배열 요소의 타입을 가져올 수 있습니다.
+// typeof와 결합하면 편리하게 배열 리터럴의 요소 타입을 캡쳐할 수 있습니다.
+type Student = typeof Users[number]
