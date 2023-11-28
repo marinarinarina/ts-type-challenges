9d0aef34191916464dea146dd4f8baa1f4198798
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2023-11-28 23:59:27 +0900

    Update 4-pick.ts

--- easy/4-pick.ts
+++ easy/4-pick.ts
@@ -1,5 +1,6 @@
-// 인덱스 시그니처, keyof, in, K extends keyof T, 리터럴 타입
-
+// 객체 T의 모든 속성을 유니온 타입으로 받는 K 타입이라면, K in keyof T겠지만,
+// 객체 T의 일부 속성을 유니온 타입으로 받는다고 나와있기 때문에 K extends keyof T가 맞다.
+// 타입스크립트에서 타입은 가능한 값의 집합이다. 따라서 extends는 T의 '확장'이 아니라 T의 '부분 집합'으로 봐야 한다.
 type MyPick<T, K extends keyof T> = {
   // [key: string]: number <- 이렇게 쓰면 키 딱 하나만 가져오게 됨. 객체의 모든 키를 가져와야 한다면 in을 쓴다.
   [key in K]: T[key] // MyPick 타입은 키는 제네릭으로 들어오는 객체의 키, 값은 그 객체의 값이다. 
